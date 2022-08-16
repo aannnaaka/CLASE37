803715ec6cba271379f7bd512f7244a921282b24
aannnaaka <93240349+aannnaaka@users.noreply.github.com>
2022-08-16 15:50:39 -0500

    Add files via upload

---
+++ Clase37Alumno/js/Player.js
@@ -0,0 +1,38 @@
+class Player {
+  constructor() {
+    this.name = null;
+    this.index = null;
+    this.positionX = 0;
+    this.positionY = 0;
+  }
+
+  addPlayer() {
+    var playerIndex = "players/player" + this.index;
+
+    if (this.index === 1) {
+      this.positionX = width / 2 - 100;
+    } else {
+      this.positionX = width / 2 + 100;
+    }
+
+    //Agregar en la base de datos un ID para cada jugador
+    
+  }
+
+  //crear una función para crear el contador
+
+ 
+ //Actualiza el contador
+  updateCount(count) {
+    database.ref("/").update({
+      playerCount: count
+    });
+  }
+
+  static getPlayersInfo() {
+    var playerInfoRef = database.ref("players");
+    playerInfoRef.on("value", data => {
+      allPlayers = data.val();
+    });
+  }
+}
